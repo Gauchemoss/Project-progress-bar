3ef266cc813f8af943bd19a585a8891c11639055
Gauchemoss <uvrugbyprofset97@gmail.com>
2021-02-10 15:47:28 +0100

    "Designed the progress bar"

--- main.go
+++ main.go
@@ -5,67 +5,64 @@ import (
 		"time"
 )
 
-type Bar struct {
-	percent int64 // progress precentage
-	cur int64	  // current progress
-	total int64	  // total value for progress
-	rate string	  // the actual progress bar to be printed
-	graph string  // the fill value for progress bar
+//Variables that are needed for creating the progress bar.
+type Progress_bar struct {
+	percent int64 
+	current_iteration_index int64	  
+	total int64	  
+	rate string	  
+	fill_progressbar_char string  
 }
 
-func (bar *Bar) NewOption(total int64)  {
-	bar.cur = 0
+//
+func (bar *Progress_bar) Default(total int64) {
+	bar.current_iteration_index = 0
+	bar.percent = 0
 	bar.total = total
-	if bar.graph == "" {
-		bar.graph = "█"
+}
+
+func (bar *Progress_bar) Progressbar_sign_exists(progress_char string)  {
+	if progress_char == "" {
+		bar.fill_progressbar_char = "█"
+	}else{
+		bar.fill_progressbar_char = progress_char
 	}
-	bar.percent = bar.getPercent()
-	for i := 1; i < int(bar.percent); i += 2 {
-		bar.rate += bar.graph // initial progress position
-		fmt.Println(bar.rate)
-	}
-	
 }
 
-func (bar *Bar) getPercent() int64 {
-	return int64(float32(bar.cur) / float32(bar.total) * 100)
+func (bar *Progress_bar) Count_percent() int64 {
+	fmt.Println(int64(float32(bar.current_iteration_index) / float32(bar.total) * 100))
+	return int64(float32(bar.current_iteration_index) / float32(bar.total) * 100)
 }
 
-func (bar *Bar) NewOptionWithGraph(start, total int64, graph string) {
-	bar.graph = graph
-	bar.NewOption(total)
-}
-
-func (bar *Bar) Play(cur int64) {
-	bar.cur = cur
+func (bar *Progress_bar) Print(current_iteration_index int64) {
+	bar.current_iteration_index = current_iteration_index
 	last := bar.percent
-	bar.percent = bar.getPercent()
+	bar.percent = bar.Count_percent()
 
-	if bar.percent != last {
-		var i int64 = 0
+	var i int64 = 0
 
-		for ; i < bar.percent - last; i++ {
-			bar.rate += bar.graph
-		}
-		fmt.Printf("\r[%-50s]%3d %% %8d/%d", bar.rate, bar.percent, bar.cur, bar.total)
+	for ; i < bar.percent - last; i++ {
+		bar.rate += bar.fill_progressbar_char
 	}
-	
-}
 
-func (bar *Bar) Finish() {
+	fmt.Printf("\r╔══════════╗")
+	fmt.Printf("\r║%-100s║ %3d %8d / %d", bar.rate, bar.percent, bar.current_iteration_index, bar.total)
+	fmt.Printf("\r╚══════════╝")
 	fmt.Println()
 }
 
-func progressBar(nrOfLoops int64) {
-	var bar Bar
-	bar.NewOption(nrOfLoops)
-	for i := 0; i <= int(nrOfLoops); i++ {
-		time.Sleep(100*time.Millisecond)
-		bar.Play(int64(i))
+func progressBar(nrOfLoops int64, progress_char string) {
+	var bar Progress_bar
+
+	bar.Default(nrOfLoops);
+	bar.Progressbar_sign_exists(progress_char)
+
+	for i := 0; i <= int(bar.total); i++ {
+		time.Sleep(50*time.Millisecond)
+		bar.Print(int64(i))
 	}
-	bar.Finish()
 }
 
 func main() {
-	progressBar(25)
+	progressBar(25, "▌")
 }
