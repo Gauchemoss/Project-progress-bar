a721c92ab47967fd7d9a71195691ce2a17b8755c
Gauchemoss <uvrugbyprofset97@gmail.com>
2021-02-15 17:00:04 +0100

    check if filled characters going out of frame

--- main.go
+++ main.go
@@ -11,11 +11,11 @@ type Progress_bar struct {
 	current_iteration_index int64	  
 	total int64	  
 	rate string	  
-	fill_progressbar_char string  
+	fill_progressbar_char string
+	fill_counter int
 }
 
-//
-func (bar *Progress_bar) Default(total int64) {
+func (bar *Progress_bar) Default_values(total int64) {
 	bar.current_iteration_index = 0
 	bar.percent = 0
 	bar.total = total
@@ -30,7 +30,6 @@ func (bar *Progress_bar) Progressbar_sign_exists(progress_char string)  {
 }
 
 func (bar *Progress_bar) Count_percent() int64 {
-	fmt.Println(int64(float32(bar.current_iteration_index) / float32(bar.total) * 100))
 	return int64(float32(bar.current_iteration_index) / float32(bar.total) * 100)
 }
 
@@ -39,30 +38,35 @@ func (bar *Progress_bar) Print(current_iteration_index int64) {
 	last := bar.percent
 	bar.percent = bar.Count_percent()
 
-	var i int64 = 0
-
-	for ; i < bar.percent - last; i++ {
-		bar.rate += bar.fill_progressbar_char
+	//check if the rate go out of frame.
+	if bar.fill_counter < 50 {
+		for i := int64(0); i < bar.percent - last; i += 2 {
+			bar.rate += bar.fill_progressbar_char
+			bar.fill_counter++
+		}
 	}
+	
 
-	fmt.Printf("\r╔══════════╗")
-	fmt.Printf("\r║%-100s║ %3d %8d / %d", bar.rate, bar.percent, bar.current_iteration_index, bar.total)
-	fmt.Printf("\r╚══════════╝")
+	fmt.Printf("\r╔══════════════════════════════════════════════════╗")
+	fmt.Printf("\r║%-50s║ %3d %% %4d / %d", bar.rate, bar.percent, bar.current_iteration_index, bar.total)
+	fmt.Printf("\r╚══════════════════════════════════════════════════╝")
 	fmt.Println()
 }
 
 func progressBar(nrOfLoops int64, progress_char string) {
 	var bar Progress_bar
 
-	bar.Default(nrOfLoops);
+	bar.Default_values(nrOfLoops);
 	bar.Progressbar_sign_exists(progress_char)
 
 	for i := 0; i <= int(bar.total); i++ {
-		time.Sleep(50*time.Millisecond)
+		time.Sleep(5*time.Millisecond)
 		bar.Print(int64(i))
 	}
 }
 
 func main() {
-	progressBar(25, "▌")
+	//progressBar(26, "")
+	//progressBar(50, "")
+	progressBar(100, "")
 }
